1daa298573b578162aaced8decfe60e813bc1b2c
blackrider <krzysztof@baralkiewicz.pl>
2020-11-21 21:03:51 +0100

    15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -9,16 +13,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping(value = "getTasks")
     public List<TaskDto> getTasks(){
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(value = "getTask")
     public TaskDto getTask(Long taskId){
-        return new TaskDto(1L, "test title", "test_content");
+        Task task = service.getTaskById(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @DeleteMapping(value = "deleteTask")
