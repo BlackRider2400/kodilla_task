f62b63a0b5eee900e5a85f4eca137d8008225efa
blackrider <krzysztof@baralkiewicz.pl>
2020-11-22 17:12:29 +0100

    15.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,9 +5,11 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,24 +28,28 @@ public class TaskController {
     }
 
     @GetMapping(value = "getTask")
-    public TaskDto getTask(Long taskId){
-        Task task = service.getTaskById(taskId);
-        return taskMapper.mapToTaskDto(task);
+    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(
+                service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
+        );
     }
 
     @DeleteMapping(value = "deleteTask")
-    public void deleteTask(Long taskId){
-
+    public void deleteTask(@RequestParam Long taskId){
+        service.deleteTask(taskId);
     }
 
-    @PostMapping(value = "updateTask")
-    public TaskDto updateTask(TaskDto task){
-        return new TaskDto(1L, "Edited test title", "Test content");
+    @PutMapping(value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto){
+        Task task = taskMapper.mapToTask(taskDto);
+        Task savedTask = service.saveTask(task);
+        return taskMapper.mapToTaskDto(savedTask);
     }
 
-    @PutMapping(value = "createMapping")
-    public void createTask(TaskDto task){
-
+    @PostMapping(value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto){
+        Task task =  taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
     }
 
 }
