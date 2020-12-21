91598ae27d27771e6122df98580458634ea8bc78
BlackRider <krzysztof@baralkiewicz.pl>
2020-12-21 10:34:10 +0100

    19.1

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/v1/trello")
 @RestController
