4ce88a75066474a2ac769cdce9a6c445a6565361
BlackRider <krzysztof@baralkiewicz.pl>
2021-01-08 23:12:21 +0100

    21.1

---
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCardDto.java
@@ -0,0 +1,28 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreatedTrelloCardDto {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("badges")
+    private BadgesDto badges;
+
+
+
+}
