a5ebde28ccc9048b40741bf998666602cb22ff39
BlackRider <krzysztof@baralkiewicz.pl>
2021-01-24 20:47:27 +0100

    24.3

--- src/main/java/com/crud/tasks/sheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/sheduler/EmailScheduler.java
@@ -2,11 +2,15 @@ package com.crud.tasks.sheduler;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.service.SimpleEmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,39 +21,13 @@ public class EmailScheduler {
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
 
-    @Scheduled(cron = "0 0 10 * * *")
+    //@Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail(){
 
-        long size = taskRepository.count();
-        if(size == 0){
-            simpleEmailService.send(
-                    new Mail(
-                            adminConfig.getAdminMail(),
-                            SUBJECT,
-                            "Currently in database you don't have any tasks.",
-                            null
-                    )
-            );
-        }
-        else if(size == 1){
-            simpleEmailService.send(
-                    new Mail(
-                            adminConfig.getAdminMail(),
-                            SUBJECT,
-                            "Currently in database you got: " + size + " task.",
-                            null
-                    )
-            );
-        }
-        else{
-            simpleEmailService.send(
-                    new Mail(
-                            adminConfig.getAdminMail(),
-                            SUBJECT,
-                            "Currently in database you got: " + size + " tasks",
-                            null
-                    )
-            );
-        }
+        List<Task> taskDtoList = taskRepository.findAll();
+        Mail mail = new Mail(adminConfig.getAdminMail(), "Tasks info", "", null);
+        simpleEmailService.sendScheduled(mail);
+
     }
 }
